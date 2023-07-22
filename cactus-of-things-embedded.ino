c3532da22bb1b63532ca4fbd7f35519de2bbf9f8
Jacob Klimczak <jacob.klimczak@gmail.com>
2023-07-21 20:14:26 -0400

    Add enterprise flag

--- cactus-of-things-embedded.ino
+++ cactus-of-things-embedded.ino
@@ -11,6 +11,11 @@
 
 const char* ssid = "ssid";
 
+/*
+ * Whether to connect to WPA enterprise, or not
+ */
+const bool enterprise = true;
+
 WiFiClient client;
 
 const int LIGHT_PIN = 35;
@@ -37,12 +42,14 @@ void connectWifi(bool debug) {
     
     WiFi.mode(WIFI_STA);
     
-    esp_wifi_sta_wpa2_ent_set_identity((uint8_t *)EAP_IDENTITY, strlen(EAP_IDENTITY));
-    esp_wifi_sta_wpa2_ent_set_username((uint8_t *)EAP_IDENTITY, strlen(EAP_IDENTITY));
-    esp_wifi_sta_wpa2_ent_set_password((uint8_t *)EAP_PASSWORD, strlen(EAP_PASSWORD));
-    
-    esp_wpa2_config_t config = WPA2_CONFIG_INIT_DEFAULT();
-    esp_wifi_sta_wpa2_ent_enable(&config);
+    if (enterprise) {
+      esp_wifi_sta_wpa2_ent_set_identity((uint8_t *)EAP_IDENTITY, strlen(EAP_IDENTITY));
+      esp_wifi_sta_wpa2_ent_set_username((uint8_t *)EAP_IDENTITY, strlen(EAP_IDENTITY));
+      esp_wifi_sta_wpa2_ent_set_password((uint8_t *)EAP_PASSWORD, strlen(EAP_PASSWORD));
+
+      esp_wpa2_config_t config = WPA2_CONFIG_INIT_DEFAULT();
+      esp_wifi_sta_wpa2_ent_enable(&config);
+    }
 
     if (debug) {
       // print mac address
@@ -51,7 +58,11 @@ void connectWifi(bool debug) {
     }
 
     // connect
-    WiFi.begin(ssid);
+    if (enterprise) {
+      WiFi.begin(ssid);
+    } else {
+      WiFi.begin(ssid, EAP_PASSWORD);
+    }
     
     int attempts = 0;
 
